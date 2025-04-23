865409ee7daed88242736e64148a1c6ffb84edc6
Alejo Chavez <alejo.chavez@alumnos.frgp.utn.edu.ar>
2025-04-23 17:01:28 -0300

    Solucionado mensaje de excepcion

--- utilidades.py
+++ utilidades.py
@@ -1,6 +1,6 @@
 import os
 import pandas
-from tkinter import Tk, filedialog
+from tkinter import Tk, filedialog, messagebox
 
 class Utilidad:
     def __init__(self):
@@ -10,80 +10,101 @@ class Utilidad:
         self.df_consolidado = None
     
     def seleccionar_directorio(self):
-        root = Tk()
-        root.withdraw()
-        rutaArchivo = filedialog.askdirectory(title="Selecciona una carpeta con archivos Excel")
-        self.ruta = rutaArchivo
+        try:
+            root = Tk()
+            root.withdraw()
+            rutaArchivo = filedialog.askdirectory(title="Selecciona una carpeta con archivos Excel")
+            self.ruta = rutaArchivo
+        except Exception as e:
+            messagebox.showerror("Error al seleccionar Datos Fuente", f"{e}")
    
     @staticmethod
     def guardar_Como(defecto="reporte.xlsx"):
-        archivo = filedialog.asksaveasfilename(title="Guardar archivo como...", defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")], initialfile=defecto)
-        return archivo
+        try:
+            #Abre el explorador de windows para elegir nombre y destino del archivo
+            archivo = filedialog.asksaveasfilename(title="Guardar archivo como...", defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")], initialfile=defecto)
+            #devuelve la ruta elegida
+            return archivo
+        except Exception as e:
+            messagebox.showerror("Error al elegir nombre y destino del archivo", f"{e}")
     
     def contar_Libros_Excel(self):
-        #Definir Tipos de Archivo admisibles
-        tipos = ('.xlsx', '.xls', '.xlsm')
-        #Guardar en una lista todos los archivos de este directorio que cumplan con la extension de archivo
-        archivos = [f for f in os.listdir(self.ruta) if os.path.isfile(os.path.join(self.ruta, f)) and f.lower().endswith(tipos)]
-        #Devolver tamaño
-        return len(archivos)
+        try:
+            #Definir Tipos de Archivo admisibles
+            tipos = ('.xlsx', '.xls', '.xlsm')
+            #Guardar en una lista todos los archivos de este directorio que cumplan con la extension de archivo
+            archivos = [f for f in os.listdir(self.ruta) if os.path.isfile(os.path.join(self.ruta, f)) and f.lower().endswith(tipos)]
+            #Devolver tamaño
+            return len(archivos)
+        except Exception as e:
+            messagebox.showerror("Error al contar Libros Excel", f"{e}")
     
     def devolver_DataFrame_De_Los_Archivos_En_Este_Directorio(self):
-        #Definir Tipos de Archivo admisibles
-        tipos = ('.xlsx', '.xls', '.xlsm')
-        #Guardar en una lista todos los archivos de este directorio que cumplan con la extension de archivo
-        if not self.ruta:
-            print("error en devolver_dataframe_de_los_archivos_en_este_directorio")
-            exit()
-        archivos = [f for f in os.listdir(self.ruta) if os.path.isfile(os.path.join(self.ruta, f)) and f.lower().endswith(tipos)]
+        try:
+            #Definir Tipos de Archivo admisibles
+            tipos = ('.xlsx', '.xls', '.xlsm')
+            #Guardar en una lista todos los archivos de este directorio que cumplan con la extension de archivo
+            if not self.ruta:
+                print("error en devolver_dataframe_de_los_archivos_en_este_directorio")
+                exit()
+            #Recorrer todos los archivos del directorio en busqueda de los .xlsx
+            archivos = [f for f in os.listdir(self.ruta) if os.path.isfile(os.path.join(self.ruta, f)) and f.lower().endswith(tipos)]
 
-        listaDataframes = []
+            listaDataframes = []
+            #Por cada Archivo del Directorio
+            for file in archivos:
+                path_completo = os.path.join(self.ruta, file)
 
-        for file in archivos:
-            path_completo = os.path.join(self.ruta, file)
-
-            try:
-                # Leer todas las hojas del archivo
-                hojas = pandas.read_excel(path_completo, sheet_name=None, usecols="A,B,C,D,E")
-
-                for nombre_hoja, df in hojas.items():
-                    df['Archivo'] = file
-                    df['Hoja'] = nombre_hoja
-                    #Agregar a la lista de los dataframes
-                    listaDataframes.append(df)
-            except Exception as e:
-                print(f"Error leyendo '{file}': {e}")
-        return listaDataframes
-    
+                try:
+                    # Leer todas las hojas del archivo
+                    hojas = pandas.read_excel(path_completo, sheet_name=None, usecols="A,B,C,D,E")
+                    #Por cada DataFrame(hojas) se agrega responsable del archivo y nombre de hoja
+                    for nombre_hoja, df in hojas.items():
+                        df['Archivo'] = file
+                        df['Hoja'] = nombre_hoja
+                        #Agregar a la lista de los dataframes
+                        listaDataframes.append(df)
+                except Exception as e:
+                    messagebox.showerror(f"Error leyendo", "Error leyendo '{file}': {e}")
+            return listaDataframes
+        except Exception as e:
+            messagebox.showerror("Error en devolver DataFrames del directorio", f"{e}")
+        
     @staticmethod
     def consolidar_Dataframes(listaDataFrames):
-        dataFramesLimpios = []
-        for dataframe in listaDataFrames:
-            #Unificar Nombres para las columnas
-            dataframe.columns = ['Nombre', 'Codigo', 'Cantidad', 'Stock o Cliente', 'Observacion', 'Archivo', 'Hoja']
-            #Quitar los registros con Cantidad Vacia
-            dataframe = dataframe[dataframe['Cantidad'].notna()]
-            #Agregar dataframe a la lista de dataframes limpios
-            dataFramesLimpios.append(dataframe)
-        #concatenar lista de dataframes limpios
-        consolidado = pandas.concat(dataFramesLimpios, ignore_index=True)
-        #retornar dataframe consolidado
-        return consolidado
+        try:
+            dataFramesLimpios = []
+            for dataframe in listaDataFrames:
+                #Unificar Nombres para las columnas
+                dataframe.columns = ['Nombre', 'Codigo', 'Cantidad', 'Stock o Cliente', 'Observacion', 'Archivo', 'Hoja']
+                #Quitar los registros con Cantidad Vacia
+                dataframe = dataframe[dataframe['Cantidad'].notna()]
+                #Agregar dataframe a la lista de dataframes limpios
+                dataFramesLimpios.append(dataframe)
+            #concatenar lista de dataframes limpios
+            consolidado = pandas.concat(dataFramesLimpios, ignore_index=True)
+            #retornar dataframe consolidado
+            return consolidado
+        except Exception as e:
+            messagebox.showerror("Error en Consolidar DataFrames", f"{e}")
     
     @staticmethod
     def agrupar_datos_DataFrame(dataFrame):
-        #Agrupar por codigo y luego por Stock o cliente, la cantidad se totaliza
-        dataFrame = dataFrame.groupby(['Codigo','Stock o Cliente'])['Cantidad'].sum().reset_index()
-        #Ordenar por Stock o Cliente
-        dataFrame = dataFrame.sort_values(by='Stock o Cliente', ascending=True)
-        return dataFrame
+        try:
+            #Agrupar por codigo y luego por Stock o cliente, la cantidad se totaliza
+            dataFrame = dataFrame.groupby(['Codigo','Stock o Cliente'])['Cantidad'].sum().reset_index()
+            #Ordenar por Stock o Cliente
+            dataFrame = dataFrame.sort_values(by='Stock o Cliente', ascending=True)
+            return dataFrame
+        except Exception as e:
+           messagebox.showerror("Error al Agrupar Archivos", f"{e}") 
     
     @staticmethod
     def exportar_excel(dataframe, nombreNuevoArchivo):
         try:
             dataframe.to_excel(nombreNuevoArchivo, index=False)
         except Exception as e:
-            print(f"Error al exportar archivo {nombreNuevoArchivo} : {e}")
+            messagebox.showerror("Error al exportar", f"Error al exportar archivo {nombreNuevoArchivo} : {e}")
         
 
 
